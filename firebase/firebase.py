bbf77f2917a3dd1a330fb68239467739dd32505c
imdyangs <d65yang@gmail.com>
2017-09-16 03:43:17 -0400

    finished basic params

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -3,7 +3,10 @@ from functools import partial
 #from aee.... import worker
 
 config = {
+    "apiKey": "AIzaSyCz-lRDfhixFe7Rsqis_5GRzwGw2Qk0ySA",
+    "authDomain": "vrml-c8ee5.firebaseapp.com",
     "databaseURL": "https://vrml-c8ee5.firebaseio.com/",
+    "storageBucket": "vrml-c8ee5.appspot.com/",
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -26,10 +29,11 @@ def finding_similar(db, store, input_coordinate, coordinates, images, distance_s
         images: list of image paths
         distance_scores: list of floats
     """
-    zipped_attributes = zip(coordinates, images, distance_scores)
+    zipped_attributes = list(zip((i for i in range(len(images))), coordinates, images, distance_scores))
     db.child("images_top_100").push(zipped_attributes)
-
+    
+    map(lambda idx, image: store.child("{}".format(idx)).put("{}".format(image)), enumerate(images))
 
 
 visualize_neurons = partial(update_neurons, database)
-find_similar = partial(finding_neurons, database)
+find_similar = partial(finding_similar, database, storage)
