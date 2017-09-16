cc92c26f07c1c3cf402bb481adea7d2b7f87d32f
Spenser Solys <solysspenser@gmail.com>
2017-09-16 16:45:07 -0400

    Work

--- unity_vrml/Assets/Scripts/FirebaseTest.cs
+++ unity_vrml/Assets/Scripts/FirebaseTest.cs
@@ -10,6 +10,7 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Unity.Editor;
+using SimpleJSON;
 
 public class FirebaseTest : MonoBehaviour
 {
@@ -21,8 +22,7 @@ public class FirebaseTest : MonoBehaviour
     FirebaseStorage storage;
     public GameObject player;
 
-    void Start()
-    {
+    void Start() {
         imagesHolder = GameObject.Find("ImagesHolder").transform;
         defaultColor = Color.blue;
         defaultScale = new Vector3(0.01f, 0.01f, 0.01f);
@@ -31,61 +31,90 @@ public class FirebaseTest : MonoBehaviour
         storage = FirebaseStorage.DefaultInstance;
         reference = FirebaseDatabase.DefaultInstance.RootReference;
         reference.Child("images_similar_100").ChildChanged += HandleChildChanged;
-       // reference.Child("message").ValueChanged += MsgChanged;
-       // reference.Child("message").ChildChanged += TstMsgChanged;
+        reference.Child("neurons").Child("activations").ValueChanged += HandleNeuronChanges;
+        reference.Child("is_training").ValueChanged += TrainingChanged;
     }
 
-    private void writeBool(bool _state)
-    {
+    bool start = true;
+    void TrainingChanged(object sender, ValueChangedEventArgs args) {
+        Debug.Log(args.Snapshot.GetRawJsonValue());
+        if(args.Snapshot.Value.ToString() == "0" && !start) {
+            hideNeurons();
+            reference.Child("query_coord").SetValueAsync("0,0,0");
+        } else if (start) {
+            start = false;
+        }
+    }
+
+    private void writeBool(bool _state) {
         State state = new State(_state);
         string json = JsonUtility.ToJson(state);
-        reference.Child("ml-state").SetRawJsonValueAsync(json);
+        reference.Child("is_training").SetRawJsonValueAsync(json);
     }
-
-    void HandleChildAdded(object sender, ChildChangedEventArgs args)
-    {
-        if (args.DatabaseError != null)
-        {
-            Debug.LogError(args.DatabaseError.Message);
-            return;
-        }
-        Debug.Log(args.Snapshot.Child("coordinates").Child("x").ToString() + ", " +
-            args.Snapshot.Child("coordinates").Child("y").ToString() + ", " +
-            args.Snapshot.Child("coordinates").Child("z").ToString());
-
-        createCube(string2Vector3(args.Snapshot.Child("coordinates").Child("x").ToString(), 
-            args.Snapshot.Child("coordinates").Child("y").ToString(), 
-            args.Snapshot.Child("coordinates").Child("z").ToString()),
-            args.Snapshot.Child("id").ToString().Replace(".png", ""));
-    }
-
-    string[] cubes;
 
     Vector3 string2Vector3(string x, string y, string z) {
-        Debug.Log(x + ", " + y + ", " + z);
         Vector3 toRet = new Vector3(float.Parse(x), float.Parse(y), float.Parse(z));
         return toRet;
     }
-    void HandleChildChanged(object sender, ChildChangedEventArgs args) {
 
+    int colCounter = 0;
+    int rowCounter = 0;
+    Dictionary<string, GameObject> neuronObjects;
 
-        Debug.Log(args.Snapshot.GetRawJsonValue());
+    void HandleNeuronChanges(object sender, ValueChangedEventArgs args) {
+        var n = JSON.Parse(args.Snapshot.GetRawJsonValue());
+        neuronObjects = new Dictionary<string, GameObject>();
 
-        //dataPoint[] dp = JsonUtility.FromJson<dataPoint>(args.Snapshot.GetRawJsonValue());
-        //dp.coords = 
-            /*updateCube(string2Vector3(args.Snapshot.Child("coordinates").Child("x").ToString(),
-            args.Snapshot.Child("coordinates").Child("y").ToString(),
-            args.Snapshot.Child("coordinates").Child("z").ToString()),
-            args.Snapshot.Child("id").ToString().Replace(".png", ""),
-            float.Parse(args.Snapshot.Child("distance").ToString()) * Color.red);*/
+        for (int k = 0; k < n.Count -1; k++) {
+            GameObject g = GameObject.Find(k.ToString());
+            if(!g) {
+                createCube(new Vector3(0 + (float)(.03 * rowCounter), 0, 0 + (float)(.03 * colCounter)), k.ToString());
+                colCounter++;
+                colCounter = colCounter % 25;
+                if (colCounter == 0)
+                    rowCounter++;
+            } else {
+                changeNColor(g, float.Parse(n[k].ToString()));
+            }
+        }
     }
 
-    void createCube(Vector3 pos, string name)
-    {
+    void hideNeurons() {
+        imagesHolder.gameObject.SetActive(false);
+    }
+
+    void deleteNeurons() {
+        foreach (Transform child in imagesHolder.transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    void changeNColor(GameObject g, float val) {
+        g.GetComponent<Renderer>().material.color = Color.blue;
+       g.GetComponent<Renderer>().material.color += new Color(0, 0, val * 10);
+    }
+
+    void HandleChildChanged(object sender, ChildChangedEventArgs args) {
+        deleteNeurons();
+        var n = JSON.Parse(args.Snapshot.GetRawJsonValue());
+        for(int i = 0; i < n.Count; i++) {
+            Debug.Log(n.AsArray[i]["id"]);
+            Debug.Log(n.AsArray[i]["coord"]);
+            Debug.Log(n.AsArray[i]["distance"]);
+
+            if (!GameObject.Find(n.AsArray[i]["id"])) {
+                createCube(string2Vector3(n.AsArray[i]["coord"]["x"], n.AsArray[i]["coord"]["y"], n.AsArray[i]["coord"]["z"]) / 5f, n.AsArray[i]["id"]);
+            } else {
+                updateCube(string2Vector3(n.AsArray[i]["coord"]["x"], n.AsArray[i]["coord"]["y"], n.AsArray[i]["coord"]["z"]) / 5f, n.AsArray[i]["id"], Color.red * n.AsArray[i]["distance"]);
+            }
+        }
+    }
+
+    GameObject createCube(Vector3 pos, string name) {
         GameObject c = GameObject.CreatePrimitive(PrimitiveType.Cube);
         c.transform.SetParent(imagesHolder);
         c.transform.localPosition = pos;
-        c.transform.localScale = defaultScale;
+        c.transform.localScale = defaultScale * 3;
         c.GetComponent<Renderer>().material.color = defaultColor;
         c.name = name;
         c.tag = "InteractionCube";
@@ -93,17 +122,17 @@ public class FirebaseTest : MonoBehaviour
         // rigidbody, no gravity
         Rigidbody c_rigidbody = c.AddComponent<Rigidbody>();
         c_rigidbody.useGravity = false;
+        c_rigidbody.freezeRotation = true;
+        c_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        return c;
     }
-
-    void updateCube(Vector3 pos, string name, Color color)
-    {
+    void updateCube(Vector3 pos, string name, Color color) {
         GameObject c = GameObject.Find(name);
         c.transform.localPosition = pos;
         c.GetComponent<Renderer>().material.color = color;
     }
 
-    void getImageFromStorage(string imageId, string cubeid)
-    {
+    void getImageFromStorage(string imageId, string cubeid) {
         StorageReference reference = storage.GetReference("images/" + imageId + ".png");
         reference.GetDownloadUrlAsync().ContinueWith(task => {
             StartCoroutine(getImageViaWWW(task.Result.ToString(), cubeid));
